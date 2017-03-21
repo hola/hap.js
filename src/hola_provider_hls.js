f2e355e16871a14b773c114be9ede4e1d2515fc9
Andrey Ivlev <andreyi@hola.org>
2017-03-21 18:43:42 +0200

    fix Hls init order

--- src/hola_provider_hls.js
+++ src/hola_provider_hls.js
@@ -1,5 +1,6 @@
+var Hls = window.Hls = require('@hola.org/hls.js').default;
 var provider = module.exports = require('__PROVIDER__');
-window.Hls = provider.Hls = require('@hola.org/hls.js').default;
+provider.Hls = Hls;
 provider.version = '__VERSION__';
 provider.hls_version = provider.Hls.version;
 provider.provider_version = provider.VERSION;
