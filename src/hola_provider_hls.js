81288ef15740b77382556b0bfd37f03ed8216f3b
Volodymyr Tytar <volodymyr.tytar@gmail.com>
2017-05-18 10:28:32 +0300

    wrap provider init with try/catch to detect init failures

--- src/hola_provider_hls.js
+++ src/hola_provider_hls.js
@@ -1,6 +1,9 @@
-var Hls = window.Hls = require('@hola.org/hls.js').default;
-var provider = module.exports = require('__PROVIDER__');
-provider.Hls = Hls;
-provider.version = '__VERSION__';
-provider.hls_version = provider.Hls.version;
-provider.provider_version = provider.VERSION;
+try {
+    var Hls = window.Hls = require('@hola.org/hls.js').default;
+    var provider = module.exports = require('__PROVIDER__');
+    provider.Hls = Hls;
+    provider.version = '__VERSION__';
+    provider.hls_version = provider.Hls.version;
+    provider.provider_version = provider.VERSION;
+}
+catch(e){ module.exports = {init_failure: e}; }
